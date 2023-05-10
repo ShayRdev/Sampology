0c2d51c7882aedc262759af987234f79ce2ae56d
guerrillaonline <shayrodriguez0@gmail.com>
2023-05-10 17:58:27 -0500

    manytomany added

--- sampology/main_app/models.py
+++ sampology/main_app/models.py
@@ -2,17 +2,6 @@ from django.db import models
 from django.urls import reverse
 # Create your models here.
 
-class Song(models.Model):
-    artist = models.CharField(max_length=100)
-    name = models.CharField(max_length=100)
-    release_date = models.DateField()
-
-    def __str__(self):
-        return self.name
-
-    def get_absolute_url(self):
-        return reverse("index")
-    
 class Gear(models.Model):
     gear_name = models.CharField(max_length=100)
     gear_brand = models.CharField(max_length=100)
@@ -24,4 +13,17 @@ class Gear(models.Model):
     
     def get_absolute_url(self):
         return reverse("gears_detail", kwargs={'pk': self.id})
+    
+
+class Song(models.Model):
+    artist = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
+    release_date = models.DateField()
+    gear_used = models.ManyToManyField(Gear)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("index")
     
